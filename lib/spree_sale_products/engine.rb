07f547e115de77e8226f80ac9d6e8f34f7080afa
Michael Bianco <iloveitaly@gmail.com>
2012-08-03 15:44:58 -0400

    Engine updates (woops). Adding install generator

---
+++ lib/spree_sale_products/engine.rb
@@ -0,0 +1,17 @@
+module SpreeSaleProducts
+  class Engine < Rails::Engine
+    require 'spree/core'
+    isolate_namespace Spree
+    engine_name 'spree_sale_products'
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.env.production? ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare &method(:activate).to_proc
+  end
+end
