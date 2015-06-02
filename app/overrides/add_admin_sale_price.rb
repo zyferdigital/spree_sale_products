22532ca5f0d7339ffe0a6a88fcc218b1180caa7e
Pedro Pinto <pedropinto.inbox@gmail.com>
2015-06-02 17:04:19 +0100

    Adjusted add_admin_sale_price.rb override

--- app/overrides/add_admin_sale_price.rb
+++ app/overrides/add_admin_sale_price.rb
@@ -4,9 +4,11 @@ Deface::Override.new(
   :insert_top => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
   :sequence => 10,
   :text => %q{
-  <%= f.field_container :sale_price do %>
-    <%= f.label :sale_price, t(:sale_price) %><br/>
-    <%= f.text_field :sale_price %>
-    <%= f.error_message_on :sale_price %>
-  <% end %>
+  <div data-hook="admin_product_form_sale_price">
+    <%= f.field_container :sale_price, class: ['form-group'] do %>
+      <%= f.label :sale_price, Spree.t(:sale_price) %>
+      <%= f.text_field :sale_price, value: number_to_currency(@product.sale_price, unit: ''), class: 'form-control' %>
+      <%= f.error_message_on :sale_price %>
+    <% end %>
+  </div>
 })
