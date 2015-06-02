93822d84b23995e951a7dacd86a2ad4b158f5176
Pedro Pinto <pedropinto.inbox@gmail.com>
2015-06-02 17:18:55 +0100

    Fix typo on :insert_before

--- app/overrides/add_admin_variant_sale_price.rb
+++ app/overrides/add_admin_variant_sale_price.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   :virtual_path => "spree/admin/variants/_form",
   :name => "add_admin_variant_sale_price",
-  :insert_before => "[data-hook='price'],
+  :insert_before => "[data-hook='price']",
   :text => %q{
   <div class="form-group" data-hook="sale_price">
     <%= f.label :sale_price, Spree.t(:sale_price) %>
