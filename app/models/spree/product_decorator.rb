488430888ca9bab8810ef2afa0ceff5d8e601e94
Pedro Pinto <pedropinto.inbox@gmail.com>
2015-06-02 12:58:52 +0100

    Removed attr_accessible :sale_price

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Product.class_eval do
   delegate_belongs_to :master, :sale_price
-  attr_accessible :sale_price
+  #attr_accessible :sale_price
 
   def on_sale?
     self.variants_including_master.inject(false) { |f, v| f || v.on_sale? }
