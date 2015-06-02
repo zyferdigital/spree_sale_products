91d8fdc66420718d02cf6a9ef24b08fb36970084
Pedro Pinto <pedropinto.inbox@gmail.com>
2015-06-02 17:27:07 +0100

    add sale_price partial reference

--- app/overrides/add_admin_variant_sale_price.rb
+++ app/overrides/add_admin_variant_sale_price.rb
@@ -1,10 +1,6 @@
 Deface::Override.new(
   :virtual_path => "spree/admin/variants/_form",
   :name => "add_admin_variant_sale_price",
-  :insert_before => "[data-hook='price']",
-  :text => %q{
-  <div class="form-group" data-hook="sale_price">
-    <%= f.label :sale_price, Spree.t(:sale_price) %>
-    <%= f.text_field :sale_price, :value => number_to_currency(@variant.sale_price, :unit => ''), :class => 'form-control' %>
-  </div>
-})
+  :insert_before => '[data-hook="price"]',
+  :partial => 'spree/admin/variants/sale_price'
+)
