e3678980e4aaf49732677459ad2ee215fe8850c1
redglory <pedropinto.inbox@gmail.com>
2015-06-02 13:05:32 -0400

    fix spree namespace and moved products sale_price to partial

--- app/overrides/add_admin_sale_price.rb
+++ app/overrides/add_admin_sale_price.rb
@@ -3,12 +3,5 @@ Deface::Override.new(
   :name => "add_admin_sale_price",
   :insert_top => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
   :sequence => 10,
-  :text => %q{
-  <div data-hook="admin_product_form_sale_price">
-    <%= f.field_container :sale_price, class: ['form-group'] do %>
-      <%= f.label :sale_price, Spree.t(:sale_price) %>
-      <%= f.text_field :sale_price, value: number_to_currency(@product.sale_price, unit: ''), class: 'form-control' %>
-      <%= f.error_message_on :sale_price %>
-    <% end %>
-  </div>
-})
+  :partial => 'spree/admin/products/sale_price'
+)
